8581e1fe519f43d71aa411bf0cfcae95498d902c
Sajad Norovzi <sajad@gpu8.cse.cuhk.edu.hk>
2017-07-19 22:38:45 +0800

    tuning parameters

--- parameters.py
+++ parameters.py
@@ -1,15 +1,15 @@
 
 HP = {
 	'e': 1,
-	'reducing_e_freq': 10**4,
+	'reducing_e_freq': 10**5,
 	'y': 0.99,
 	'num_episodes': 2500,
 	'size_of_experience' : 10**6,
-	'mini_batch_size' : 64,
-	'target_update': 10**3,
+	'mini_batch_size' : 32,
+	'target_update': 10**4,
 	'learning_rate': 0.001,
 	'regularization_factor': 0.001,
-	'initial_experience_sizes': 700,
+	'initial_experience_sizes': 5*10**4,
 	'stacked_frame_size': 4
 }
 
