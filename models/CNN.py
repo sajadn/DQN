0cbb5992bcd95c629359ded9f1315ea06862aa3a
Sajad Norouzi <sajadnorouzi@Sajads-MacBook-Pro.local>
2017-07-31 18:57:44 +0800

    change CNN totally

--- models/CNN.py
+++ models/CNN.py
@@ -8,6 +8,8 @@ from scipy.misc import imresize, imsave
 from itertools import chain
 from ..models.modelBase import modelBase
 from ..models.DQNBaseModel import DQNBaseModel
+from tensorflow.contrib.layers.python.layers import initializers
+
 
 
 
@@ -30,36 +32,36 @@ class CNN(DQNBaseModel):
             		momentum=0.95)
 
 	def defineNNArchitecture(self):
-		conv1 = tf.layers.conv2d(
-				inputs= self.X,
-				filters=32,
-				strides=[4, 4],
-				kernel_size=[8, 8],
-				name="L1",
-				activation=tf.nn.relu)
-
-		conv2 = tf.layers.conv2d(
-				inputs= conv1,
-				filters=64,
-				strides=[2, 2],
-				name="L2",
-				kernel_size=[4, 4],
-				activation=tf.nn.relu)
-
-		conv3 = tf.layers.conv2d(
-				inputs=conv2,
-				filters=64,
-				name="L3",
-				kernel_size=[3, 3],
-				activation=tf.nn.relu)
-
-		flatten = tf.reshape(conv3, [-1, 7 * 7 * 64])
-		dense = tf.layers.dense(inputs=flatten, units=512, activation=tf.nn.relu, name="L4")
-		self.Qprime = tf.layers.dense(inputs=dense, units=self.output_size, name="L5")
+		l1, w1, b1 = conv2d(self.X, 32, [8, 8], [4, 4], name='l1')
+		l2, w2, b2 = conv2d(l1, 64, [4, 4], [2, 2], name='l2')
+		l3, w3, b3 = conv2d(l2, 64, [3, 3], [1, 1], name='l3')
+		shape = l3.get_shape().as_list()
+      	l3_flat = tf.reshape(l3, [-1, reduce(lambda x, y: x * y, shape[1:])])
+		l4, w4, b4 = linear(l3_flat, 512, activation_fn=tf.nn.relu, name='l4')
+        self.Qprime, w5, b5 = linear(l4, self.output_size, name='l5')
 		self.P = tf.argmax(self.Qprime, 1)
 		self.Qmean = tf.reduce_mean(tf.reduce_max(self.Qprime,axis = 1))
 		self.QmeanSummary = tf.summary.scalar("Qmean", self.Qmean)
-		tfGraph = tf.get_default_graph()
-		k = lambda x: tfGraph.get_tensor_by_name('L{}/kernel:0'.format(x))
-		b = lambda x: tfGraph.get_tensor_by_name('L{}/bias:0'.format(x))
-		self.weights = list(chain.from_iterable((k(x), b(x)) for x in range(1,6)))
+		self.weights = [w1, b1, w2, b2, w3, b3, w4, b4, w5, b5]
+
+	def conv2d(x, output_dim, kernel_size, stride, name='conv2d', data_format='NHWC'):
+		with tf.variable_scope(name):
+			stride = [1, stride[0], stride[1], 1]
+			kernel_shape = [kernel_size[0], kernel_size[1], x.get_shape()[-1], output_dim]
+			w = tf.get_variable('w', kernel_shape, tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+	    	conv = tf.nn.conv2d(x, w, stride, "Valid", data_format=data_format)
+			b = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+			out = tf.nn.bias_add(conv, b, data_format)
+			out = tf.nn.relu(out)
+		return out, w, b
+
+	def linear(input_, output_size, activation_fn, name='linear'):
+		shape = input_.get_shape().as_list()
+		with tf.variable_scope(name):
+			w = tf.get_variable('W', [shape[1], output_size], tf.float32, tf.random_normal_initializer(stddev=0.02))
+			b = tf.get_variable('B', [output_size], initializer=tf.constant_initializer(0.0))
+	    	out = tf.nn.bias_add(tf.matmul(input_, w), b)
+		if activation_fn != None:
+			return activation_fn(out), w, b
+		else:
+			return out, w, b
